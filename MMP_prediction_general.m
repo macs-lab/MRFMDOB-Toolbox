77c6f7d85028186979edccbd3a7c21e5a6dd45ec
xiaohuits <hui.xiao@uconn.edu>
2020-07-05 17:40:00 -0400

    Add prediction step parameters to MMP

--- MMP_prediction_general.m
+++ MMP_prediction_general.m
@@ -1,8 +1,8 @@
-% An example usage of MMP simulink modual.
+% An example usage of MMP for prediction, general model
 %==========================================================================
 % Copyright (c) 2019 Hui Xiao
 %==========================================================================
-% 3-7-2019
+% 3-20-2019
 
 % In this example, the MMP will predict future data (k steps)
 addpath('functions')
@@ -13,39 +13,32 @@ Tsf = 0.008;   % fast smapling period
 L = 6;
 Tss = Tsf*L;     % slow sampling period
 Tsc = Tsf/10;  % sampling time for the continuous signal
-n_amp = 0;   % noise amplitude
-t_run = 5;    % simulation running time
+n_amp = 0.001;   % noise amplitude
+t_run = 20;    % simulation running time
 %% define siganl frequency distribution
-f = [1.2 3.1];
-Amp = [1 0.6];
-Phi = [0.6 0];
+% constant model
+A_const = [1 -1];
+% linear model
+A_linear = [1 -2 1];
+% quadratic model
+A_quad = [1 -3 3 -1];
 
+Apara = A_quad;
 %% Calculate prediction parameters
 predic_step = 7;
-[B,a] = IIR_MMP(f,L,Tss,0.95,predic_step);  % IIR parameters
-Apara = Apara_prd(f,Tsf); % calculate the signal model
 m = length(Apara)-1;
 p = m - 1; % give unique solution.
 W = FIR_MMP(Apara, L, p, predic_step);
 
 %% run simulation
-sim('MMP_simulink_prediction_example');
+sim('MMP_simulink_prediction_general_example');
 
 %% plot results
 figure,
 plot(signal_c.time, signal_c.signals.values);
 hold on,
 stairs(measurement.time, measurement.signals.values);
-stairs(FIR_output.time, FIR_output.signals.values);
+stairs(FIR_output.time, FIR_output.signals.values(:));
 legend('true signal','measurement','recovered signal');
 xlabel('time (seconds)');
 title('FIR-MMP');
-
-figure,
-plot(signal_c.time, signal_c.signals.values);
-hold on,
-stairs(measurement.time, measurement.signals.values);
-stairs(IIR_output.time, IIR_output.signals.values);
-legend('true signal','measurement','recovered signal');
-xlabel('time (seconds)');
-title('IIR-MMP');
